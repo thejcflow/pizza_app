a0b8e7e3862162edd86d5abba69f4ebdabcf7922
Juan Chávez <im.jhcc@gmail.com>
2020-03-03 14:29:05 -0400

    Creating route for lasagna resource.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :products do
       resources :pizzas, only: [:index, :show, :create]
+      resources :lasagna, only: [:index, :show, :create]
     end
   end
 end
