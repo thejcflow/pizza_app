3f3f9f56f78810e747d29cd8318f2fcf8fda16d8
Juan Chávez <im.jhcc@gmail.com>
2020-03-03 14:23:30 -0400

    Creating route for pizzas resource.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :products do
+      resources :pizzas, only: [:index, :show, :create]
+    end
+  end
 end
