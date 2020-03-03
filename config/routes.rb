82424e5a05d89816fc682bbd33e2395788554046
Juan Chávez <im.jhcc@gmail.com>
2020-03-03 14:35:10 -0400

    Creating route for spaghetti resource.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
       resources :pizzas, only: [:index, :show, :create]
       resources :lasagna, only: [:index, :show, :create]
       resources :salads, only: [:index, :show, :create]
+      resources :spaghetti, only: [:index, :show, :create]
     end
   end
 end
