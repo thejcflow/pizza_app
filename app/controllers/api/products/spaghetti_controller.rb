b16b3d8006a044626bf6257cf297b07496737739
Juan Chávez <im.jhcc@gmail.com>
2020-03-03 14:34:06 -0400

    Creating first version of spaghetti controller.

---
+++ app/controllers/api/products/spaghetti_controller.rb
@@ -0,0 +1,21 @@
+class Api::Products::SpaghettiController < Api::Products::OrderController
+  ## Mocked wait time for updating orders.
+  @@wait_time = 10
+
+  def index
+    super(Spaghetti)
+  end
+
+  def show
+    super(Spaghetti)
+  end
+
+  def create
+    super(Spaghetti, spaghetti_params, @@wait_time)
+  end
+
+  private
+  def spaghetti_params
+    params.permit(:deliver, :state)
+  end
+end
