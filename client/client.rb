546647cedbc9bcaf38159595d615e0b3f64f0e9c
Juan Chávez <im.jhcc@gmail.com>
2020-03-03 14:44:47 -0400

    Updating client class using generic query generator.

--- client/client.rb
+++ client/client.rb
@@ -1,6 +1,7 @@
 require 'faraday'
 require 'json'
 require './pizza_query_generator'
+require './generic_query_generator'
 
 class Client
   @@base_url = "/api/products"
@@ -12,6 +13,13 @@ class Client
   def self.generate_pizza_request
     url = "#{@@hostname + @@base_url}/pizzas"
     body = PizzaQueryGenerator.generate_post_body
+    headers = {"Content-Type" => "application/json"}
+    Faraday.post(url, body, headers)
+  end
+
+  def self.generate_generic_request
+    url = "#{@@hostname + @@base_url}#{["/spaghetti", "/lasagna", "/salads"].sample}"
+    body = GenericQueryGenerator.generate_post_body
     headers = {"Content-Type" => "application/json"}
     Faraday.post(url, body, headers)
   end
